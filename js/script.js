b5463fc543e622c9e27dadea86b2921b48734511
DarioGiusino <dario.giusino@gmail.com>
2022-12-07 15:34:19 +0100

    1-100 cycle

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/* Steps
+1- get element from dom(bonus 1)
+2- creare ciclo che generi numeri da 1 a 100
+3a- implementare, se numero multiplo di 3, scritta "Fizz" al posto del numero
+3b- implementare, se numero multiplo di 5, scritta "Buzz" al posto del numero
+3c- implementare, se numero multiplo di 3 *E* 5, scritta "FizzBuzz" al posto del numero
+----
+*/
+
+// // get element from dom
+
+// 2- ciclo 1-100
+for (let i = 1; i <=100; i++){
+    console.log(i);
+}
